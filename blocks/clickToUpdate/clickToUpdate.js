f5fd7aefd6916e54ac5c82be1cb69ca235e8f56a
Pawan Godwani <pawan.godwani@tadigital.com>
2024-11-19 00:31:11 +0530

    fixing build issues

--- blocks/clickToUpdate/clickToUpdate.js
+++ blocks/clickToUpdate/clickToUpdate.js
@@ -1,32 +1,32 @@
 export default function decorate(block) {
-  const mainDiv = document.createElement('div');
-  mainDiv.classList.add('mainDiv');
-  const rows = [...block.children];
-
-  const leftDiv = (document.createElement('div'));
-  leftDiv.classList.add('leftDiv');
-
-  rows.forEach((row) => {
-    const leftDivItem = document.createElement('div');
-    leftDivItem.classList.add('leftDivItem');
-    leftDivItem.textContent = row.children[0].textContent.toUpperCase();
-    leftDiv.append(leftDivItem);
-  });
-
-  mainDiv.append(leftDiv);
-  const resDiv = document.createElement('div');
-  resDiv.classList.add('rightDiv');
-
-  const response = document.createElement('div');
-  response.classList.add('response');
-  resDiv.append(response);
-
-  mainDiv.append(resDiv);
-
-  block.textContent = '';
-  block.append(mainDiv);
-
-  leftDiv.addEventListener('click', (e) => {
-    response.textContent = `${(e.target.textContent).toLowerCase()} clicked`;
-  });
-}
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('main-div');
+    const rows = [...block.children];
+  
+    const leftDiv = (document.createElement('div'));
+    leftDiv.classList.add('left-div');
+  
+    rows.forEach((row) => {
+      const leftDivItem = document.createElement('div');
+      leftDivItem.classList.add('left-div-item');
+      leftDivItem.textContent = row.children[0].textContent.toUpperCase();
+      leftDiv.append(leftDivItem);
+    });
+  
+    mainDiv.append(leftDiv);
+    const resDiv = document.createElement('div');
+    resDiv.classList.add('right-div');
+  
+    const response = document.createElement('div');
+    response.classList.add('response');
+    resDiv.append(response);
+  
+    mainDiv.append(resDiv);
+  
+    block.textContent = '';
+    block.append(mainDiv);
+  
+    leftDiv.addEventListener('click', (e) => {
+      response.textContent = `${(e.target.textContent).toLowerCase()} clicked`;
+    });
+  }
